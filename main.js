614597cae908094b812bc0d18e201cf9f6c299a9
DexterFreeman <67049776+DexterFreeman@users.noreply.github.com>
2023-01-17 16:36:44 +0000

    Added comments

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
+//Intersection observer observes any components that are currently visible to the user/on screen.
+//When these components come into view, .show is added to that components class, which triggers a CSS animation,
+//Which can be viewed in /styling/style.scss.
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting){
-            entry.target.classList.add('show');
-        }
-        else {
-            entry.target.classList.remove('show');
-        }
-    });
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
+//Gets all elements with the .hidden class to pass into the intersection observer.
+const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
